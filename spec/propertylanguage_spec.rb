714e0daae375ce418e0eb1b4bad8194a58b14808
bp50fathoms <bp50fathoms@gmail.com>
2009-04-16 19:30:00 +0200

    Property language integrated, specs extended to 100% C0

---
+++ spec/propertylanguage_spec.rb
@@ -0,0 +1,74 @@
+require 'property_helpers'
+
+
+module PropertyLanguageSpec
+  describe Kernel do
+    it_should_behave_like 'Property'
+
+    it 'should build a property with => notation' do
+      p = property :p => [String, String] do |a,b|
+        a == b
+      end
+      p.key.should == :p
+      p.types.should == [String, String]
+      p.call('a', 'a').should be_true
+    end
+
+    it 'should build a sandboxed property' do
+      p = property :p do
+        false
+      end
+      p.key.should == :p
+      p.types.should == []
+      p.call.should be_false
+    end
+
+    it 'should build a property with => notation and without explicit list' do
+      p = property :p => String do |a|
+        a.length == 0
+      end
+      p.key.should == :p
+      p.types.should == [String]
+      p.call('').should be_true
+    end
+
+    it '' do
+      doc = 'Example doc'
+      desc doc
+      p = property :p => [String, String] do |a,b|
+        a == b
+      end
+      p.desc.should == doc
+    end
+
+    it 'should reject a property with a long hash in its signature' do
+      lambda do
+        property :p1 => String, :p2 => [String] do |a|
+          a.length == a.size
+        end
+      end.should raise_error(ArgumentError)
+    end
+  end
+
+
+  describe TrueClass do
+    it 'true => true = true' do
+      (true.implies true).should be_true
+    end
+
+    it 'true => false = false' do
+      (true.implies false).should be_false
+    end
+  end
+
+
+  describe FalseClass do
+    it 'false => true = true' do
+      (false.implies true).should be_true
+    end
+
+    it 'false => false = true' do
+      (false.implies false).should be_true
+    end
+  end
+end
---
+++ src/property_language.rb
@@ -0,0 +1,48 @@
+require 'property'
+
+
+module Kernel
+  def desc(doc)
+    Property.next_desc = doc
+  end
+
+  def property(signature, &block)
+    Property.new(*Signature::dump_signature(signature), &block)
+  end
+end
+
+
+module Signature
+  def dump_signature(signature)
+    if signature.is_a?(Hash)
+      dump_hash(signature)
+    else
+      [signature, []]
+    end
+  end
+
+  def dump_hash(hash)
+    if hash.size != 1
+      raise ArgumentError, 'incorrect property signature'
+    end
+    ary = hash.to_a
+    types = ary.first.last
+    [ary.first.first, types.is_a?(Array) ? types : [types]]
+  end
+
+  module_function :dump_signature, :dump_hash
+end
+
+
+class TrueClass
+  def implies(conseq)
+    !(conseq.nil? or conseq == false)
+  end
+end
+
+
+class FalseClass
+  def implies(conseq)
+    true
+  end
+end

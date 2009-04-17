4f56ab3695e2eb33e56dab8b6daa8db4b5ff7493
bp50fathoms <bp50fathoms@gmail.com>
2009-04-17 20:30:31 +0200

    PropertyVisitor changed to add param

--- spec/propertyvisitor_spec.rb
+++ spec/propertyvisitor_spec.rb
@@ -1,23 +1,26 @@
-require 'property_helpers'
+require 'property'
 
 
 module PropertyVisitorSpec
   describe PropertyVisitor do
-    it_should_behave_like 'Property'
-
-    it 'should reject empty properties' do
-      p = Property.new(:p, [String]) { |a| }
-      lambda { PropertyVisitor.new(p) }.should raise_error(RuntimeError)
+    it 'should reject empty blocks' do
+      lambda do
+        PropertyVisitor.proc(lambda { |a| })
+      end.should raise_error(RuntimeError)
     end
 
-    it 'should ' do
-      p = Property.new(:p, [TrueClass]) { |a| a }
-      PropertyVisitor.new(p)
+    it 'should add an additional parameter for instrumentation' do
+      source(PropertyVisitor.proc(lambda { |a| a })).should ==
+        'proc { |a, _r| a }'
     end
 
-    it '' do
-      p = Property.new(:p, [String]) { |a| a.length == 0 }
-      PropertyVisitor.new(p)
+    it 'should add an additional parameter for instrumentation' do
+      source(PropertyVisitor.proc(lambda { |a,b| a & b })).should ==
+        'proc { |a, b, _r| a.&(b) }'
+    end
+
+    def source(o)
+      Ruby2Ruby.new.process(o.to_sexp)
     end
   end
 end
--- src/property_visitor.rb
+++ src/property_visitor.rb
@@ -6,28 +6,43 @@ require 'parse_tree_extensions'
 
 
 class PropertyVisitor < SexpProcessor
-  def initialize(property)
-    super()
-    proc_body(property.predicate.to_sexp)
+  def self.proc(block)
+    ncode = Ruby2Ruby.new.process(self.new.proc_body(block.to_sexp))
+    block.binding.eval(ncode)
   end
 
   def proc_body(exp)
-    3.times { exp.shift }
+    exp.shift
+    call = exp.shift
+    asgn = exp.shift
     raise 'Empty property body' if exp.empty?
-    exp.shift
-    process(exp.shift)
+    s(:iter, call, add_arg(asgn), exp.shift)
   end
 
-  def process_lvar(exp)
-    exp.shift
-    exp.shift
-    s()
-  end
+  # protected
 
-  def process_call(exp)
-    while !exp.empty?
-      exp.shift
+  # def process_lvar(exp)
+  #   exp.shift
+  #   exp.shift
+  #   s()
+  # end
+
+  # def process_call(exp)
+  #   while !exp.empty?
+  #     exp.shift
+  #   end
+  #   s()
+  # end
+
+  private
+
+  def add_arg(asgn)
+    param = s(:lasgn, :_r)
+    if asgn[0] == :lasgn
+      s(:masgn, s(:array, asgn, param))
+    else
+      asgn[1] << param
+      asgn
     end
-    s()
   end
 end

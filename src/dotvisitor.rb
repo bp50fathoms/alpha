acba4ff11763f30bda3f4e82ea5f580be50eaf0d
bp50fathoms <bp50fathoms@gmail.com>
2009-04-21 17:43:38 +0200

    Minimal DOTVisitor implementation

---
+++ src/dotvisitor.rb
@@ -0,0 +1,75 @@
+require 'rubygems'
+require 'graphviz'
+
+
+class DOTVisitor
+  attr_reader :graph, :property
+
+  def initialize(graph, property)
+    @graph = graph
+    @property = property
+    @nodes = {}
+    property.tree.accept(self)
+  end
+
+  def visit_unaryexpr(e)
+    add_node(e, e.operator.to_s)
+    visit_and_link(e, e.expr)
+  end
+
+  def visit_binaryexpr(e)
+    add_node(e, e.operator.to_s)
+    visit_and_link(e, e.left_expr)
+    visit_and_link(e, e.right_expr)
+  end
+
+  def visit_quantexpr(e)
+    add_node(e, e.type.to_s)
+    visit_and_link(e, e.expr)
+  end
+
+  def visit_conditional(e)
+    add_node(e, 'if')
+    visit_and_link(e, e.condition)
+    visit_and_link(e, e.then_branch)
+    visit_and_link(e, e.else_branch)
+  end
+
+  def visit_composition(e)
+    visit(Property[e.property].tree)
+  end
+
+  def visit_boolconst(e)
+    add_node(e, e.value.to_s)
+  end
+
+  def visit_boolatom(e)
+    add_node(e, 'atom')
+  end
+
+  private
+
+  def key(object)
+    object.object_id.to_s
+  end
+
+  def visit_and_link(parent, child)
+    add_edge(parent, visit(child))
+    parent
+  end
+
+  def add_node(node, label)
+    @nodes[node.object_id] = @graph.add_node(key(node), { :label => label,
+                                               :color => 'green',
+                                               :style => 'filled' })
+    node
+  end
+
+  def add_edge(parent, child)
+    @graph.add_edge(key(parent), key(child))
+  end
+
+  def visit(object, *args)
+    object.accept(self, *args)
+  end
+end

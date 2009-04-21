07d8d5c53e1ae16fe1268a2338fc4b3e00110bbf
bp50fathoms <bp50fathoms@gmail.com>
2009-04-21 11:47:04 +0200

    Property cases integrated

---
+++ spec/propertycases_spec.rb
@@ -0,0 +1,56 @@
+require 'property_helpers'
+
+
+module PropertyCasesSpec
+  describe Property do
+    include PropertyHelpers
+
+    it_should_behave_like 'Property'
+
+    it 'should accept a simple sequence of cases to be checked' do
+      p = property :p => String do
+        predicate { |s| s.size == s.length }
+
+        always_check '', "\\\?"
+      end
+      p.cases.should == [[''], ["\\\?"]]
+    end
+
+    it 'should accept one case to be checked' do
+      p = property :p => String do
+        predicate { |s| s.size == s.length }
+
+        always_check ''
+      end
+      p.cases.should == [['']]
+    end
+
+    it 'should accept a simple sequence in a property with arity > 1' do
+      p = property :p => [String, String] do
+        predicate { |a, b| a == b }
+
+        always_check ['', ''], ['a', 'a']
+      end
+      p.cases.should == [['', ''], ['a', 'a']]
+    end
+
+    it 'should accept one case in a property with arity > 1' do
+      p = property :p => [String, String] do
+        predicate { |a, b| a == b }
+
+        always_check ['', '']
+      end
+      p.cases.should == [['', '']]
+    end
+
+    it 'should reject a number of cases with wrong arity' do
+      lambda do
+        p = property :p => String do
+          predicate { |s| s.size == s.length }
+
+          always_check ['', '']
+        end
+      end.should raise_error(ArgumentError)
+    end
+  end
+end
---
+++ src/property_cases.rb
@@ -0,0 +1,14 @@
+require 'property'
+
+
+class Property
+  attr_reader :cases
+
+  def always_check(*cases)
+    c = cases.map { |e| e.is_a?(Array) ? e : [e] }
+    unless c.map { |e| e.size == arity }.inject(:&)
+      raise ArgumentError, "wrong number of cases for arity #{arity}"
+    end
+    @cases = c
+  end
+end

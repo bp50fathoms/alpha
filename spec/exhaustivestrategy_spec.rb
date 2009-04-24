d9f06fe11c5926132ffaf0db5ff4def849c85ec6
bp50fathoms <bp50fathoms@gmail.com>
2009-04-24 14:36:07 +0200

    Half of new contract specs added to Exhaustive spec

--- spec/exhaustivestrategy_spec.rb
+++ spec/exhaustivestrategy_spec.rb
@@ -51,7 +51,7 @@ module ExhaustiveStrategySpec
       end
 
 
-      describe ExhaustiveStrategy, 'with a property that has many cases, arity =2' do
+      describe ExhaustiveStrategy, 'with a property that has many cases, arity=2' do
         def define_prop
           property :p => [String, String] do |a,b| a == b end
         end
@@ -71,6 +71,51 @@ module ExhaustiveStrategySpec
           @strategy.generate.should == ["\1", "\0"]
           @strategy.exhausted?.should be_false
           @strategy.progress.should == 0.5
+        end
+      end
+
+
+      describe ExhaustiveStrategy, 'with a contract that does not have any cases' do
+      end
+
+
+      describe ExhaustiveStrategy, 'with a contract that has many cases' do
+        class Foo
+          def bar(a)
+            true
+          end
+
+          def self.exhaustive(n)
+            [self.new]
+          end
+        end
+
+        def define_prop
+          Contract.new(Foo.instance_method(:bar), [String]) do
+            requires { |s| true }
+            ensures { |r,s| true }
+          end
+        end
+
+        it_should_behave_like 'PropertyWithCases'
+
+        it 'should generate correctly all tuples of increasing sizes' do
+          check(@strategy.generate, '')
+          @strategy.exhausted?.should be_false
+          @strategy.progress.should == 0.5
+          check(@strategy.generate, "\0")
+          127.times do
+             s = @strategy.generate
+          end
+          @strategy.progress.should == 1.0
+          @strategy.exhausted?.should be_false
+          check(@strategy.generate, "\0\0")
+        end
+
+        def check(t, s)
+          t.size.should == 2
+          t.first.is_a?(Foo).should be_true
+          t.last.should == s
         end
       end
     end
